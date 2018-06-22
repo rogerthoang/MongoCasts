fe410513181e00e7b43c6c01c26ca97283beb926
Roger Hoang <RHoang@aag.com>
2018-06-22 16:45:20 -0700

    added new Mongoose tutorial project

---
+++ MongoWorkspace/users/test/create_test.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongoosedb://localhost/users_test');
+mongoose.connection
+  .once('open', () => {
+    console.log('Good to go!');
+  })
+  .on('error', error => {
+    console.warn('Warning', error);
+  });
